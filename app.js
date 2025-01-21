04fa4cfa4afa03abf6819f4289727c310fac3f1f
Edwin Mejia <elopez75290@gmail.com>
2025-01-20 19:51:28 -0600

    Se creó funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+   // console.log(nombre);
+    if (nombre == 0) {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombre);
+        //console.log(amigos); 
+    }
+}
+
+
+    
