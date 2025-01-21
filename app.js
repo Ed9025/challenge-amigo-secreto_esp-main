83bdb29ac1781424fdcc00b7d85d99f2e02749eb
Edwin Mejia <elopez75290@gmail.com>
2025-01-21 15:49:42 -0600

    Crea funcion para actualizar lista de amigos

--- app.js
+++ app.js
@@ -4,17 +4,39 @@ let amigos = [];
 
 function agregarAmigo() {
     let nombre = document.getElementById("amigo").value;
-   // console.log(nombre);
+    // console.log(nombre);
     if (nombre == 0) {
         alert("Por favor, inserte un nombre.");
     } else {
         amigos.push(nombre);
-       // console.log(amigos);
+        // console.log(amigos);
         limpiarInput();
     }
+    return
 }
 
 
 function limpiarInput() {
     document.querySelector("#amigo").value = "";
+    return
+}
+
+function actualizarTextoHTML(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML += `<li>${texto}</li>`
+    return
+
+
+}
+
+function listarAmigos() {
+    //limpia la lista de amigos antes de agregar nuevos
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    //itinerancia en cada elemento contenido en array
+    for (let i of amigos) {
+        actualizarTextoHTML("listaAmigos", i);
+
+    }
+    return
 }
